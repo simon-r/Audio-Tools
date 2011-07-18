7a1aa6f80b76f126aaacb6d1da62d36a65ec405e
simon-r <simone.rva@gmail.com>
2011-07-18 19:07:15 +0200

    added gain_set

--- u_rms.m
+++ u_rms.m
@@ -13,21 +13,7 @@ ref = p.Results.ref ;
 
 t = 1/FS ;
 
-if time_range(1) ~= -1
-    if time_range(2) < time_range(1)
-        error('error: negative time range') ;
-    end
-    
-    if time_range(2) < 0 || time_range(1) < 0
-        error('error: the values in time range must be positives') ;
-    end
-    
-    start_sample = floor( time_range(1) / t ) ;
-    end_sample = floor( time_range(2) / t ) ;
-else
-    start_sample = 1 ;
-    end_sample = size(Y,1) ;
-end
+[ start_sample end_sample ] = test_time_range( time_range , t , size(Y) ) ;
 
 r = start_sample:end_sample ;
 res = sqrt ( sum( Y(r,:).^2 / size( Y(r,:),1) ) ) ;
