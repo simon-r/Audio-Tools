ab9e9c74475016874f17be66148c6a1cdf495b75
simon-r <simone.rva@gmail.com>
2011-07-19 17:33:34 +0200

    updated detect clipping

--- detect_clipping.m
+++ detect_clipping.m
@@ -1,19 +1,24 @@
 function [ C clip_cnt ] = detect_clipping( Y , FS , level_dB )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-% C { ch s_begin s_end t_begin t_end clip_sign }
+% C [ ch s_begin s_end t_begin t_end clip_sign ]
 p = inputParser ;
 
-p.addOptional( 'level_dB' , 0 , @(x)isnumeric(x) && x <= 0 ) ;
-p.parse( level_dB ) ;
+
+if level_dB > 0 
+    error( 'error: the clipping level must be less or equal than 0.' )
+end
 
 level = 10^(level_dB/20) ;
 clip = false ;
 
 t = 1/FS ;
 
-C = {} ;
+sz = 20000 ;
+step = sz ;
+C = zeros(sz,6) ;
 clip_cnt = 0 ;
+
 for ch=1:size(Y,2)
     
     clip_flag = false ;
@@ -22,28 +27,35 @@ for ch=1:size(Y,2)
     
     for i=1:size(Y,1)
         
-        if ( Y(i,ch) >= level || Y(i,ch) <= -level ) && clip_flag == false
+        if not( clip_flag ) && ( Y(i,ch) >= level | Y(i,ch) <= -level )
             
             clip_flag = true ;
             clip_cnt = clip_cnt + 1 ;
             clip_begin = i ;
             clip_sign = sign(Y(i,ch)) ;
             
-        elseif ( Y(i,ch) < level || Y(i,ch) > -level ) && clip_flag
+        elseif clip_flag && ( Y(i,ch) < level & Y(i,ch) > -level )
             
             clip_flag = false ;
             
-            C{clip_cnt,1} = ch ;
-            C{clip_cnt,2} = clip_begin ;
-            C{clip_cnt,3} = i-1 ;
-            C{clip_cnt,4} = clip_begin*t ;
-            C{clip_cnt,5} = (i-1)*t ;
-            C{clip_cnt,5} = clip_sign ;
+            C(clip_cnt,1) = ch ;
+            C(clip_cnt,2) = clip_begin ;
+            C(clip_cnt,3) = i-1 ;
+            C(clip_cnt,4) = clip_begin*t ;
+            C(clip_cnt,5) = (i-1)*t ;
+            C(clip_cnt,6) = clip_sign ;
+            
+            if clip_cnt >= sz 
+                C = [C ; zeros(step,6)] ;
+                sz = size(C,1) ;
+            end
             
         end
         
     end
 end
+
+C = C(1:clip_cnt,:) ;
 
 end
 
