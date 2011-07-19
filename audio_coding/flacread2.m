6ab4ff6f275e97cd7cec42287e8c0755001b63fc
simon-r <simone.rva@gmail.com>
2011-07-19 14:48:57 +0200

    added flacread & oggread

---
+++ audio_coding/flacread2.m
@@ -0,0 +1,116 @@
+function [Y,FS,NBITS,tag_info] = flacread2(FILE,varargin)
+%    Y = flacread(FILE) reads a FLAC file specified by the string FILE,
+%    returning the sampled data in Y. Amplitude values are in the range [-1,+1].
+% 
+%    [Y,FS,NBITS,tag_info] = FLACREAD2(FILE,[n1,n2],'fmt') 
+%    fmt can be native or double (default)
+%
+%    [samples,channels] = FLACREAD2(FILE,'size')  returns samples and channels
+%
+%    Supports up to 8 channels of data, with up to 32 bits per sample.
+%
+%    By Emanuele Ruffaldi 2009/06/21
+% 
+%    See also  WAVWRITE, AUREAD, AUWRITE.
+tag_info = [];
+Y=[];
+FS=0;
+NBITS=0;
+fmt = 'double';
+a = length(FILE);
+if a >= 5
+    exten = FILE(a-4:a);
+    if exten ~= '.flac'
+        FILE = strcat(FILE,'.flac');
+    end
+end
+if a <= 4
+    FILE = strcat(FILE,'.flac');
+end
+if exist(FILE) ~= 2
+    error('File not Found')
+end
+%%%%%% Location of the ".exe" Files
+s = which('flacread2.m');
+ww = findstr('flacread2.m',s);
+location = [s(1:ww-2),filesep];
+if ~exist([location,'flac.exe']) || ~exist([location,'metaflac.exe'])
+	location ='';
+end
+
+%%%%Temporary file%%%%%%
+tmpfile = tempname;
+N12 = [];
+location='';
+for I=1:nargin-1
+	if ischar(varargin{I}) && strcmp(varargin{I},'size') 
+		[stat,data_title] = system([location,'metaflac', ' --show-total-samples --show-channels "', FILE,'"']);
+		if length(data_title) >= 2
+			YR = textscan(data_title,'%d\n%d\n');
+			Y = [YR{1},YR{2}];
+		else
+			Y = [];
+		end
+		return
+	elseif ~ischar(varargin{I})
+		N12 = varargin{I}
+		if length(N12) == 1
+			N12 = [1 N12];
+		end
+	elseif ischar(varargin{I}) && strcmp(varargin{I},'native')
+		fmt = varargin{I};
+	elseif ischar(varargin{I}) && strcmp(varargin{I},'double')
+		fmt = varargin{I};
+	else
+		error('not accepted');
+	end
+end	
+s = [location,'metaflac', ' --show-total-samples --show-channels --show-bps --show-sample-rate "', FILE,'"'];
+[stat,data_title] = system(s);
+if stat ~= 0
+	return
+end
+YR = textscan(data_title,'%d\n%d\n%d\n%d\n');
+TOT = double(YR{1});
+CH = double(YR{2});
+NBITS = double(YR{3});
+if isempty(N12)
+	N12 = [1 TOT];
+end
+if NBITS ~= 8 && NBITS ~= 16 && NBITS ~=24
+	error('Unsupported Bits');
+end
+FS = double(YR{4});
+if ~isempty(N12)
+s = [location,'flac', ' -d "', FILE ,'" -f ',' --sign=signed --endian=little  --force-raw-format ',sprintf(' --until=%d --skip=%d ',N12(2),N12(1)-1),' -o ', '"',tmpfile,'"'];
+else
+s = [location,'flac', ' -d "', FILE ,'" -f ',' --sign=signed --endian=little --force-raw-format ',' -o ', '"',tmpfile,'"'];
+end
+[stat_2,raw_data] = system(s);
+if stat_2 == 1
+    error('Error while decodong file. File may be corrupted')
+end
+fid = fopen(tmpfile,'r');
+dtype ='';
+if NBITS==8
+	dtype = 'uchar';
+elseif NBITS==16
+	dtype = 'int16';
+elseif NBITS==24
+	dtype = 'bit24';
+end
+if strcmp(fmt,'native')
+	dtype = ['*' dtype];
+end
+Y = fread(fid,[CH (N12(2)-N12(1)+1)],dtype)';
+if strcmp(fmt,'double')
+	if NBITS==16
+		Y = Y/32768; % is it safe?
+	elseif NBITS==24
+		Y = Y/(2^23);
+	elseif NBITS==8
+		Y = (Y-128)/128.0; % is it safe?
+	end
+end
+fclose(fid);
+delete(tmpfile);
---
+++ audio_coding/oggread.m
@@ -0,0 +1,69 @@
+function [Y,FS,NBITS,encoding_info,tag_info] = oggread(FILE)
+%OGGREAD Read OGG (".ogg") sound file.
+%    Y = OGGREAD(FILE) reads a OGG file specified by the string FILE,
+%    returning the sampled data in Y. Amplitude values are in the range [-1,+1].
+% 
+%    [Y,FS,NBITS,encoding_info,tag_info] = OGGREAD(FILE) returns the sample rate (FS) in Hertz
+%    and the number of bits per sample (NBITS) used to encode the
+%    data in the file.
+%
+%    'encoding_info' is a string containing information about the mp3
+%    encoding used
+%
+%    'tag_info' is a string containing the tag information of the file
+%
+% 
+%    Supports two channel or mono encoded data.
+% 
+%    See also OGGWRITE, WAVWRITE, AUREAD, AUWRITE.
+a = length(FILE);
+if a >= 4
+    exten = FILE(a-3:a);
+    if exten ~= '.ogg'
+        FILE = strcat(FILE,'.ogg');
+    end
+end
+if a <= 3
+    FILE = strcat(FILE,'.ogg');
+end
+if exist(FILE) ~= 2
+    error('File not Found')
+end
+%%%%%% Location of the ".exe" Files
+s = which('oggread.m');
+ww = findstr('oggread.m',s);
+location = '/usr/bin'
+%%%%Temporary file%%%%%%
+tmpfile = ['temp.wav'];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%% Info extraction using "ogginfo.exe"%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[stat_1,raw_info] = dos([location,'\ogginfo',' ', '"',FILE,'"']);
+raw_info_channels_beg = findstr(raw_info,'Channels: ');
+raw_info_channels_end = findstr(raw_info,'Rate: ')-2;
+info_channels = raw_info(raw_info_channels_beg:raw_info_channels_end);
+raw_info_rate_beg = findstr(raw_info,'Rate: ');
+raw_info_rate_end = findstr(raw_info,'Nominal bitrate: ')-1;
+info_rate = strcat(raw_info(raw_info_rate_beg:raw_info_rate_end),' Hz');
+raw_info_bit_rate = findstr(raw_info,'Nominal bitrate: ');
+raw_info_bit_rate_end = findstr(raw_info,'kb/s');
+info_bit_rate = raw_info(raw_info_bit_rate+17:raw_info_bit_rate_end-1);
+info_bit_rate = ['Bit Rate: ',num2str(floor(str2num(info_bit_rate))),' Kb/s'];
+encoding_info = {info_channels info_rate info_bit_rate};
+%%%%% TAG INFO %%%%%
+if isempty(findstr(raw_info,'User comments section follows...')) ~= 1
+    tag_info_beg = findstr(raw_info,'User comments section follows...')+32;
+    tag_info_end = findstr(raw_info,'Vorbis stream 1:')-1;
+    tag_info = raw_info(tag_info_beg:tag_info_end);
+else
+    tag_info = 'No Tag Info';
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%% File Decoding using "oggdec.exe" %%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[stat_2,raw_data] = dos([location,'/oggdec', ' -o ', tmpfile, ' ', '"',FILE,'"']);
+if stat_1 == 1 | stat_2 == 1
+    error('Error while decodong file. File may be corrupted')
+end
+[Y,FS,NBITS] = wavread(tmpfile);    % Load the data and delete temporary file
+delete(tmpfile);
