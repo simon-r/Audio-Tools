09283a02b7572db20ac5cd9fd76a4c00a93a9a20
simon-r <simone.rva@gmail.com>
2011-07-27 16:43:37 +0200

    edit sweep

--- frequency_sweep.m
+++ frequency_sweep.m
@@ -1,10 +1,31 @@
-function [ Y t_vect ] = frequency_sweep( FS , f_min , f_max , time )
+function [ Y t_vect ] = frequency_sweep( FS , f_min , f_max , time , varargin )
 % frequency_sweep Summary of this function goes here
 %   Detailed explanation goes here
 
+p = inputParser ;
+p.addParamValue( 'waveform', 'sin' , @(x)strcmpi(x,'sine') || ... 
+    strcmpi(x,'triangle') || ...
+    strcmpi(x,'square') || ... 
+    strcmpi(x,'sawtooth') ) ;
+
+p.addParamValue('gain', -6 , @(x)isnumeric(x) && x <= 0 ) ;
+
+p.parse(varargin{:});
+
+if strcmpi( p.Results.waveform , 'sin' ) 
+    wave_f = @sin ;
+elseif strcmpi( p.Results.waveform , 'triangle' ) 
+    wave_f = @triangle_f ;
+elseif strcmpi( p.Results.waveform , 'square' ) 
+    wave_f = @square_f ;
+elseif strcmpi( p.Results.waveform , 'sawtooth' ) 
+    wave_f = @sawtooth_f ;
+end
 
 ch = 1 ;
 t = 1/FS ;
+
+gain = p.Results.gain ;
 
 samples = floor( time / t ) ;
 Y = zeros( samples , ch ) ;
@@ -24,10 +45,22 @@ end
 t_vect = 0:delta_t:time ;
 
 for i = 1:ch
-    Y(:,i) = sin( t_v .* t_vect )' ;
+    Y(:,i) = wave_f( t_v .* t_vect )' ;
 end
 
-    Y = gain_set( Y , FS , -6 , 'dB' ) ;
+    Y = gain_set( Y , FS , gain , 'dB' ) ;
 
 end
 
+function [ tr ] = triangle_f(x)
+    tr = asin( sin( x ) ) / (pi/2) ;
+end
+
+function [ sq ] = square_f(x)
+    sq = sign( sin( x ) ) ;
+end
+
+function [ sw ] = sawtooth_f ( x ) 
+    a = 2*pi ;
+    sw = 2*( x/a - floor( x/a + 1/2 ) ) ;
+end
