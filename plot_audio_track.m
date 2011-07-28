1ee92ca9aa2341eb82d177c2c92e212383c6fade
simon-r <simone.rva@gmail.com>
2011-07-28 23:36:57 +0200

    added plot_audio_track.m

--- plot_audio_track.m
+++ plot_audio_track.m
@@ -5,10 +5,26 @@ function [ h ] = plot_audio_track( Y , FS )
 t = 1/FS ;
 sY = size( Y ) ;
 
-for i=1:sY(2)
-    
+time = t * (0:(sY(1)-1)) ;
+
+max_sam = 20000 ;
+
+if sY(1) > max_sam
+    r = find( mod( 1:sY(1) , floor( sY(1) / max_sam ) ) == 1 ) ;
+else
+    r = 1:sY(1) ;
 end
 
+
+for i=1:sY(2)
+    subplot( sY(2) , 1 , i ) ;
+    plot( time(r) , Y(r,i) ) ;
+    grid on ;
+    ti = sprintf( 'Channel: %1.0f' , i ) ;
+    title( ti ) ; 
+    xlabel('Time: [Sec]') ;
+    ylabel('Amplitude') ;
+end
 
 
 end
