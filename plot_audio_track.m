1cf9afc884dd0f8a1fb1adb98039ce96708b6947
simon-r <simone.rva@gmail.com>
2011-07-28 23:13:29 +0200

    added plot_audio_track.m

---
+++ plot_audio_track.m
@@ -0,0 +1,15 @@
+function [ h ] = plot_audio_track( Y , FS )
+%plot_audio_track plot an audio track
+%   Detailed explanation goes here
+
+t = 1/FS ;
+sY = size( Y ) ;
+
+for i=1:sY(2)
+    
+end
+
+
+
+end
+
