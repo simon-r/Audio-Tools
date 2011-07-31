9aa31c229364030e77d0b90b8540c79fa88f90e9
simon-r <simone.rva@gmail.com>
2011-07-31 10:33:42 +0200

    added: fit spectral

--- fit_spectral.m
+++ fit_spectral.m
@@ -1,7 +1,25 @@
-function [ output_args ] = fit_spectral( input_args )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [ spr g ] = fit_spectral( ref_spr , spr , freq )
+%[ spr g ] = fit_spectral( ref_spr , spr , freq )
+%   move spr along the gain axis such that the the squared difference
+%   between ref_spr and spr is minimized.
+%
+%   Arguments:
+%      ref_spr: n-by.channels matrix that stores the reference spectra
+%      spr:     n-by.channels matrix that stores the movable spectra
+%      freq:    a n-by-1 vector that stores the frequancies.
+%
+%   Returns:
+%      spr: the fitted spectra.
+%      g: 1-by-channels vector that stores the differences the new spr 
+%         and the old one.
 
+
+r = find( freq > min_freq && freq < max_freq ) ;
+g = - ( spr(r) - ref_spr(r) ) / size(  spr(r) , 1 ) ;
+
+for i=1:size(spr,2)
+    spr(:,i) = spr(:,i) + g(:,i) ;
+end
 
 end
 
