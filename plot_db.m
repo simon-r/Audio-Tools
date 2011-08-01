851a90a31450b28ff968f85583a609dee9b7d520
simon-r <simone.rva@gmail.com>
2011-08-01 09:08:36 +0200

    added plot_db.m

--- plot_db.m
+++ plot_db.m
@@ -1,4 +1,4 @@
-function [ h ] = plot_db( x , dB , x_mode , l_message , t_message )
+function [ h ] = plot_db( x , dB , x_mode , l_message , t_message , varargin )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -15,7 +15,7 @@ h = zeros(sdB(2)) ;
 
 for i=1:sdB(2)         
     subplot( sdB(2) , 1 , i ) ;
-    h(i) = plot( x , dB , 'Color' , 'blue' ) ;
+    h(i) = plot( x , dB , varargin{:} ) ;
     set( h(i) , 'YScale' , 'lin' , 'XScale' , 'lin' ) ;
     xlabel(xl_message) ;
     ylabel('dB') ;
