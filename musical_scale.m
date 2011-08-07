a70dc166bdd04a971c47fce6503591e0ae098d97
simon-r <simone.rva@gmail.com>
2011-08-07 16:02:38 +0200

    added midi functions

--- musical_scale.m
+++ musical_scale.m
@@ -2,23 +2,42 @@ function [ Y ] = musical_scale( varargin )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-a = 440 ;
+p = inputParser ;
+p.addOptional('start_key', 1, @(x)isinteger(x) && x >= 1 ) ;
+p.addOptional('end_key', 88, @(x)isinteger(x) && x >= 1 ) ;
+p.addOptional('Fs', 44100, @(x)isnumeric(x) && x > 0 ) ;
+
+p.addParamValue('note_time', 1 , @(x)isnumeric(x) && x > 0 ) ;
+p.addParamValue('gain', -16 , @(x)isnumeric(x) && x <= 0 ) ;
+p.addParamValue('channels', 1 , @(x)isinteger(x) && x > 0 ) ;
+p.addParamValue('a', 440 , @(x)isinteger(x) && x > 400 && x < 480 ) ;
+
+p.parse(varargin{:});
+
+a = p.Results.a ;
 main_a = a / 16 ;
 
-keys_cnt = 88 ;
-Fs = 44100 ;
-note_time = 1 ;
-gain = -13 ;
-ch = 1 ;
+start_key = p.Results.start_key ;
+end_key = p.Results.end_key ;
+Fs = p.Results.Fs ;
+note_time = p.Results.note_time ;
+gain =  p.Results.gain ;
+ch =  p.Results.channels ;
 
+keys_cnt = end_key - start_key + 1 ;
 Y = zeros( keys_cnt*Fs*note_time , ch ) ;
 
-for i=1:keys_cnt 
+for i=start_key:end_key
     fr = main_a * 2^((i-1)/12) ;
+    
     Yn = frequency_sweep( 44100 , fr , fr , note_time , 'gain' , gain ) ;
     
+    Yn = fade_inout( Yn , Fs , note_time/10 ) ;
+    
     r = ((i-1)*note_time*Fs+1):((i)*note_time*Fs) ;
-    Y(r,1) = Yn ;
+    for j=1:ch
+        Y(r,j) = Yn ;
+    end
 end
 
 end
