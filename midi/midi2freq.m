2ad7d8c49f57339126947eac51a40199e7a26a91
simon-r <simone.rva@gmail.com>
2011-08-07 17:44:28 +0200

    added midi functions

---
+++ midi/midi2freq.m
@@ -0,0 +1,17 @@
+function [ f ] = midi2freq( m , varargin )
+%MIDI2FREQ Summary of this function goes here
+%   Detailed explanation goes here
+
+if nargin >= 2 
+    a = varargin{1} ;
+    if a < 410 || a > 470
+        error('the a should be included between 410 Hz and 470 Hz') ;
+    end
+else
+    a = 440 ;
+end
+
+f = a/32 * 2^((m-9)/12) ;
+
+end
+
